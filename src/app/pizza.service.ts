fbc3bcb2fe21636260d0be22693e9fe6f7b03e0d
tsteelematc <tsteele@madisoncollege.edu>
2021-10-07 11:50:08 -0500

    Got the service hooked up...
    And showing pizza toppings in the console...
    
    Next, bind with *ngFor...

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromWebService() {
+
+    const ptFromWeb = [
+      {
+        name: 'Pepperoni'
+        , price: 1.75
+      }
+      , {
+        name: 'Olives'
+        , price: 2.25
+      }      
+      , {
+        // Extra cheese should be cheaper...
+        name: 'Extra Cheese'
+        , price: 0.75
+      }
+    ];
+
+    return ptFromWeb;
+  }
 }
