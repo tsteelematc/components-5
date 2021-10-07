df7792bac573d91090cab9f9b76a5ab8e4503920
aleebit <alee@madisoncollege.edu>
2021-10-07 11:50:19 -0500

    Got service hooked up---
    And showing pizza topping in the console

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ɵangular_packages_platform_browser_platform_browser_a } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingFromWebService(){
+    const ptFromWeb = [
+      {
+        name: 'Pepperroni'
+        , price: 1.75
+      }
+      , {
+        name: 'Olives'
+        , price: 2.25
+      }
+      , {
+        name: 'Extra Cheese'
+        , price: 0.75
+      }
+    ];
+
+    return ptFromWeb;
+  }
+
 }
