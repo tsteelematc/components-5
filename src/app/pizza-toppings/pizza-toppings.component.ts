8a2334858cc51cd207b5158a9eeed5fa0482feb5
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2021-10-12 11:35:21 -0500

    Reimplemented total to be calculated automatically
    Removed calculate total button

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -33,15 +33,17 @@ export class PizzaToppingsComponent implements OnInit {
     );
   }
 
-  calculateTotal = () => this.total = this.pizzaToppings
-      .filter(
-          x => x.checked
-      )
-      .reduce(
+  // Typescript "getter" property!
+  get total() {
+    return this.pizzaToppings
+        .filter(
+            x => x.checked
+        )
+        .reduce(
           (acc, x) => acc + x.price
           , 0
-      )
-  ;
-  total = 0;
+        )
+    ;
+  };
 
 }
