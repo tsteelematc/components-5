15466ab77af2d255fdaa65a6f42e300576ee2428
Yaifer <91929314+Yaifer@users.noreply.github.com>
2021-10-14 11:16:29 -0500

    Installed angular maerial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule
-    , FormsModule
+    , FormsModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
