8d78558ab8f47ce00e19d8cdb26a77cc8c9a515b
tsteelematc <tsteele@madisoncollege.edu>
2021-10-14 11:40:01 -0500

    Showing pizza toppings on a card...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule
-    , FormsModule, BrowserAnimationsModule
+    , FormsModule
+    , BrowserAnimationsModule
+    , MatCardModule
+    , MatCheckboxModule
+    , MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
