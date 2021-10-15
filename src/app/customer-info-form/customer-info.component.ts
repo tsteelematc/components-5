3d99b1f743be348f502b96abd688fbaf40ce4f0b
Tia Petts <tpetts@madisoncollege.edu>
2021-10-14 20:40:08 -0500

    Adding in Bikini Bottom Citizens
    Just so you know, I love Spongebob... So I incorporated him into our Pizza form...which coincidently could be called the Krusty Krab Pizza.
    
    I'm so doing that. Changing the name right meow.

--- src/app/customer-info-form/customer-info.component.ts
+++ src/app/customer-info-form/customer-info.component.ts
@@ -10,3 +10,17 @@ interface DisplayCustomerInfo {
     zipCode: string;
     phoneNumber: number;
 }
+
+@Component({
+    selector: 'app-customer-info-form',
+    templateUrl: './customer-info.component.html',
+    styleUrls: ['./customer-info.component.css']
+})
+export class CustomerServiceComponent implements OnInit {
+
+    // Magic ctor DI (dependency injection)
+  constructor(
+    private customerSvc: CustomerService
+  ) { }
+    
+}
--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -7,5 +7,41 @@ export class CustomerService {
 
     constructor() { }
 
-    
+    getCustomerInfoFromWebService() {
+
+        const customerFromWeb = [
+            {
+                firstName: 'Spongebob'
+                , lastName: 'Squarepants'
+                , streetAddress: '124 Conch St.'
+                , city: 'Bikini Bottom'
+                , state: 'Hawaii'
+                , zipCode: '54321'
+                , phoneNumber: 333-214-9000
+            }
+
+            , {
+                firstName: 'Patrick'
+                , lastName: 'Star'
+                , streetAddress: '120 Conch St.'
+                , city: 'Bikini Bottom'
+                , state: 'Hawaii'
+                , zipCode: '54321'
+                , phoneNumber: 333-214-9001
+            }
+
+            , {
+                firstName: 'Eugene'
+                , lastName: 'Krabs'
+                , streetAddress: '3541 Anchor Way'
+                , city: 'Bikini Bottom'
+                , state: 'Hawaii'
+                , zipCode: '54322'
+                , phoneNumber: 333-215-9111
+            }
+        ];
+
+        return customerFromWeb;
+
+    }
 }
