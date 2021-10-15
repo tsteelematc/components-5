6ccac7defea96119c9059751038ceefd22325914
Tia Petts <tpetts@madisoncollege.edu>
2021-10-14 20:06:53 -0500

    creating interface
    need to 'shape' my form data...

--- src/app/customer-info-form/customer-info.component.ts
+++ src/app/customer-info-form/customer-info.component.ts
@@ -1,2 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from "../customer.service";
+
+interface DisplayCustomerInfo {
+    firstName: string;
+    lastName: string;
+    streetAddress: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    phoneNumber: number;
+}
--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -1,1 +1,11 @@
 import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root' 
+})
+export class CustomerService {
+
+    constructor() { }
+
+    
+}
