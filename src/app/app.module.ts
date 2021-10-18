0dd6cd9bb6a23fcd15b59a6eef7db47b55eb4d8d
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-10-18 15:55:06 -0500

    Added my addition component...
    ng add component tjs-addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { TjsAdditionComponent } from './tjs-addition/tjs-addition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    TjsAdditionComponent
   ],
   imports: [
     BrowserModule
