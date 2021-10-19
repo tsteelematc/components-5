aa4c90fe37882792a2d319634efc7ec61294135a
Yaifer <91929314+Yaifer@users.noreply.github.com>
2021-10-18 23:06:48 -0500

    Generated my addition component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { JdhAdditionComponent } from './jdh-addition/jdh-addition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    JdhAdditionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/jdh-addition/jdh-addition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jdh-addition',
+  templateUrl: './jdh-addition.component.html',
+  styleUrls: ['./jdh-addition.component.css']
+})
+export class JdhAdditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
