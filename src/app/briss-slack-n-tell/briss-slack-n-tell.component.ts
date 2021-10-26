765a9c10c30c8233a4fccc3ff551e9d4851cd8c9
ClaytonDewey <clay@dryadmedia.com>
2021-10-26 11:05:59 -0500

    *ngFor Demo again ! ! !

--- src/app/briss-slack-n-tell/briss-slack-n-tell.component.ts
+++ src/app/briss-slack-n-tell/briss-slack-n-tell.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-briss-slack-n-tell',
-  templateUrl: './briss-slack-n-tell.component.html',
-  styleUrls: ['./briss-slack-n-tell.component.css']
+    selector: 'app-briss-slack-n-tell',
+    templateUrl: './briss-slack-n-tell.component.html',
+    styleUrls: ['./briss-slack-n-tell.component.css']
 })
 export class BrissSlackNTellComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    classes = [
+        "PHP"
+        , "JSF"
+        , "Agile"
+        , "Job Search"
+    ]
 }
